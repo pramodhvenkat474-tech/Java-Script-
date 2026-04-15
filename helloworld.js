a2ff834eefe77536506e59e98eaf80cea0fc6adc
Pramodh <pramodhvenkat474@gmail.com>
2026-04-15 12:35:26 +0530

    15/4/2026

---
+++ helloworld.js
@@ -0,0 +1,4 @@
+console.log("hello")
+sum of 2 Number
+div of 2 Number
+difference of 2 Number
---
+++ pramodh.js
@@ -0,0 +1,69 @@
+var a = 10;
+// var b = 20;
+// console.log("The Sum of 2 number is")
+// console.log(a+b)
+
+// a=40
+
+// console.log(a)
+
+// var a = 50; 
+
+// console.log(a)
+// VAR
+
+// * initialize
+// * value update
+// * re-initialize
+var a = 10;
+var b = 20;
+console.log("The Sum of 2 number is")
+console.log(a+b)
+
+a=40
+
+console.log(a)
+
+var a = 50; 
+
+console.log(a)
+VAR
+
+// * initialize
+// * value update
+// * re-initialize
+let a = 10;
+let b = 20
+
+console.log("The Sum of 2 number is")
+
+console.log(a+b)
+
+a = 20
+
+console.log(a)
+
+let a = 50
+
+console.log(a)
+
+
+LET
+*initialize
+* update
+// * can't able to re-initialize
+const a = 10;
+const b = 20;
+console.log(a+b)
+
+// a = 30
+// console.log(a)
+
+const a =40
+console.log(a)
+
+// const
+
+// *initialize
+// *can't able to update
+// * can't able to re-initialize
